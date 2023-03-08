d95770a5f8c9e3cf1f7f819129f4455ebb25d0de
LoganMil <95763669+LoganMil@users.noreply.github.com>
2023-03-08 13:15:19 -0500

    setup and skeleton
    setup the beginning of quiz and skeleton for questions

--- Mini Projects/03_QuizGame/main.py
+++ Mini Projects/03_QuizGame/main.py
@@ -1,2 +1,40 @@
 # From this walkthrough by Tech With Tim, link goes to timestamp for this specific project
 # https://youtu.be/DLn3jOsNRVE?t=291
+
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay, let's play :)")
+
+# Q1
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print('Correct!')
+else:
+    print("Incorrect :(")
+
+# Q2
+answer = input("")
+if answer == "":
+    print('Correct!')
+else:
+    print("Incorrect :(")
+
+# Q3
+answer = input("")
+if answer == "":
+    print('Correct!')
+else:
+    print("Incorrect :(")
+
+# Q4
+answer = input("")
+if answer == "":
+    print('Correct!')
+else:
+    print("Incorrect :(")
+
